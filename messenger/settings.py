64841cdf811714d5161b695a9c9a6618f01f8aea
yourbrolol <andriy.bondar.2203@gmail.com>
2026-05-09 15:49:34 +0300

    fix static dir path in settings; add template README.md

--- messenger/settings.py
+++ messenger/settings.py
@@ -154,4 +154,4 @@ AUTH_USER_MODEL = "accounts.User"
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / r"chat\static"]
+STATICFILES_DIRS = [BASE_DIR / "chat" / "static"]
